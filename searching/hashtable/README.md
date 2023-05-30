518603b8bf3080b66cb7ba8d592299f99d324b59
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2023-05-30 16:20:46 +0100

    hashtable

--- searching/hashtable/README.md
+++ searching/hashtable/README.md
@@ -1,2 +1,10 @@
 # Hashtable
 
+A Hashtable is a data structure that allows you to store and retrieve values based on a key. It uses a hash function to compute an index into an array of buckets or slots, from which the desired value can be found. Here's a simple implementation of a Hashtable in Python:
+
+# Code Explanation
+
+The Hashtable class has an __init__ method that initializes the hashtable with a given size and creates an empty list of buckets.
+The hash method computes the hash value of a given key and returns the index in the array of buckets.
+The put method inserts a key-value pair into the hashtable. If the bucket at the computed index is empty, it creates a new list with the key-value pair. If the bucket is not empty, it checks if the key already exists in the bucket. If it does, it updates the value; otherwise, it appends the new key-value pair to the bucket.
+The get method retrieves the value associated with a given key. It computes the index of the bucket using the hash function and then searches the bucket for the key. If the key is found, it returns the associated value; otherwise, it returns None.
--- searching/hashtable/hashtable.py
+++ searching/hashtable/hashtable.py
@@ -1,2 +1,42 @@
 # Hashtable Algorithm
 
+class Hashtable:
+    def __init__(self, size):
+        self.size = size
+        self.buckets = [None] * size
+
+    def hash(self, key):
+        return hash(key) % self.size
+
+    def put(self, key, value):
+        index = self.hash(key)
+        if self.buckets[index] is None:
+            self.buckets[index] = [(key, value)]
+        else:
+            for pair in self.buckets[index]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return
+            self.buckets[index].append((key, value))
+
+    def get(self, key):
+        index = self.hash(key)
+        if self.buckets[index] is None:
+            return None
+        for pair in self.buckets[index]:
+            if pair[0] == key:
+                return pair[1]
+        return None
+
+# Create a hashtable with a size of 10
+ht = Hashtable(10)
+
+# Put some key-value pairs into the hashtable
+ht.put("text-1", 1)
+ht.put("text-2", 2)
+ht.put("text-3", 3)
+
+# Retrieve values based on their keys
+print(ht.get("text-1"))  # Output: 1
+print(ht.get("text-2"))  # Output: 2
+print(ht.get("text-3"))  # Output: 3
