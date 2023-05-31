169c93b1d1f4106ef373cc83139f5de428c45592
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2023-05-31 14:54:53 +0100

    Concept of Dynamic programming

--- dynamicprogramming/README.md
+++ dynamicprogramming/README.md
@@ -1,2 +1,12 @@
-# Dynamicprogramming
+# Dynamic programming
+
+Dynamic Programming is mainly an optimization over plain recursion. Wherever we see a recursive solution that has repeated calls for same inputs, we can optimize it using Dynamic Programming. The idea is to simply store the results of subproblems, so that we do not have to re-compute them when needed later. This simple optimization reduces time complexities from exponential to polynomial.
+
+# Code Explanation 
+
+Dynamic programming algorithm can bu used on Fibonacci algorithm.
+
+This algorithm has a time complexity of O(n), which is much faster than the exponential time complexity of a simple recursive solution.
+
+Dynamic programming is widely used for optimization problems, where we are trying to find out the minimum or maximum solution of a problem. The dynamic programming guarantees to find the optimal solution of a problem if the solution exists.
 
--- dynamicprogramming/dynamicprogramming.py
+++ dynamicprogramming/dynamicprogramming.py
@@ -1,2 +1,42 @@
-# Dynamicprogramming Algorithm
+# Dynamic programming (example with Fibonacci algorithm )
 
+def fib(n):
+    # Create an array to store the calculated values
+    fib_values = [0, 1]
+    for i in range(2, n+1):
+        # Calculate the next value based on the previous two
+        next_value = fib_values[i-1] + fib_values[i-2]
+        # Store the value in the array
+        fib_values.append(next_value)
+    return fib_values[n]
+
+# fast
+print(fib(10))
+# fast
+print(fib(100))
+# fast
+print(fib(100000))
+
+
+#############################################################
+# Without Dynamic programming 
+# Example of a recursive function 
+# As the input size grows, 
+# the number of recalculations grows exponentially, 
+# making this function very slow for large inputs.
+
+
+def fib2(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib2(n-1) + fib2(n-2)
+
+# fast
+print(fib2(10))
+# How about we go for coffee?
+print(fib2(100))
+# not in this life 
+print(fib2(10000))
